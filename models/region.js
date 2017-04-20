a52411bc8581e6475ed99d5be40ee6a853088de8
Laura de Waard <l.dewaard@student.avans.nl>
2017-04-20 16:17:30 +0200

    fix

--- models/region.js
+++ models/region.js
@@ -12,36 +12,27 @@ console.log('Initializing region schema')
      *     required:
      *       - name
      *       - adminUser
-     *       - status
+     *       - isActive
      *       - postalCodes
-     *       - latitude
-     *       - longitude
 	 *     properties:
      *       name:
      *         type: string
      *       adminuser:
      *         type: string
      *         format: ObjectId
-     *       status:
+     *       isActive:
      *         type: boolean
      *       postalCodes:
      *         type: string
      *         format: array
-     *       latitude:
-     *         type: number      
-     *       longitude:
-     *         type: number
 	*/
 
 // define the schema for our region model
 var regionSchema = mongoose.Schema({
     name : {type: String, required: true},
-    adminUser : { type: mongoose.Schema.Types.ObjectId, ref: "User", required:true },
-    status : { type: boolean, required: true },
+    manager : { type: mongoose.Schema.Types.ObjectId, ref: "User", required:true },
+    isActive : { type: Boolean, required: true },
     postalCodes : [{type: String, required:true}],
-    latitude : {type: Number, required: true},
-    longitude : {type: Number, required: true},
-
 });
 
 // create the model for users and expose it to our app
