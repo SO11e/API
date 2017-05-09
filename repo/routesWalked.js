b985cb20a2904a38c68145d3588c9061deb557f9
Laura de Waard <l.dewaard@student.avans.nl>
2017-05-09 15:48:40 +0200

    post routeswalked

---
+++ repo/routesWalked.js
@@ -0,0 +1,72 @@
+var mongoose = require('mongoose');
+var RoutesWalked = mongoose.model('RoutesWalked');
+var User = mongoose.model('User');
+
+var routesWalkedRepo = {
+    create: function (req, res) {
+        var routesWalked = new RoutesWalked(req.body);
+        var token = req.header('bearer');
+
+        if (token) {
+            User.validToken(token, function (err, user) {
+                if (!err) {
+                    routesWalked.userId = user._id;
+                    routesWalked.save((err) => {
+                        if (err) {
+                            return res.send({
+                                "status": 500,
+                                "error": err
+                            })
+                        }
+
+                        return res.send(routesWalked);
+                    });
+                }
+            });
+        }
+    },
+
+    getall: function (req, res) {
+        var perPage = 10;
+        var page = 0 * perPage;
+
+        if (req.query.perPage != null) {
+            perPage = parseInt(req.query.perPage);
+            console.log(req.query.perPage + ' perPage');
+        }
+        if (req.query.page != null) {
+            page = parseInt(req.query.page * perPage);
+            console.log(req.query.page + ' page');
+        }
+
+        Issue.find().limit(perPage).skip(page).exec(function (err, data) {
+            if (!err) {
+                var json = [];
+                data.forEach(function (item, key) {
+                    //TODO: niet te strikt nemen!
+
+                    var thing = item;
+                    json.push(thing);
+                })
+
+                return res.send({ "page": page, "perPage": perPage, "data": json, });
+            } else {
+                throw err;
+            }
+        })
+    },
+
+    getbyregion: function (req, res) {
+        Issue.findOne({ '_id': req.params.id }, function (err, polish) {
+            if (!err) {
+                return res.send(polish);
+            }
+            else {
+                return res.send(400);
+            }
+        });
+    },
+
+}
+
+module.exports = routesWalkedRepo;
